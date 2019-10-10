ddf021874ed04ad5e38d1048f20f239601fc3f89
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2019-10-11 00:27:33 +0200

    Last Update

--- Downloads/Sublime Text Build 3176 x64/second-component/src/app/form-page/form-page.component.ts
+++ Downloads/Sublime Text Build 3176 x64/second-component/src/app/form-page/form-page.component.ts
@@ -41,14 +41,24 @@ export class FormPageComponent implements OnInit {
             return;
         } else if (this.registerForm.value.password == this.registerForm.value.confirmPassword) {
             this.register.firstName = this.registerForm.value.firstName;
-            this.register.secondName = this.registerForm.value.lastName;
+            this.register.lastName = this.registerForm.value.lastName;
             this.register.email = this.registerForm.value.email;
             this.register.password = this.registerForm.value.password;
 
                 alert('SUCCESS!!! :-)\n\n' + JSON.stringify(this.registerForm.value));
-                console.log('fistName: ' + this.register.firstName +'\n'+'Secondname: ' + this.register.secondName +'\n'+ 'email: ' + this.register.email +'\n'+ 'password: ' + this.register.password);
+                console.log('firstName: ' + this.register.firstName +'\n'+'lastName: ' + this.register.lastName +'\n'+ 'email: ' + this.register.email +'\n'+ 'password: ' + this.register.password);
                 let url = 'http://localhost:3000/signup';
-              this.http.post<Registers>(url, this.registerForm.value).subscribe(data => {
+
+              this.http.post<Registers>(url, { 
+
+              'firstName': this.register.firstName,
+              'lastName': this.register.lastName,
+              'email': this.register.email,
+              'password': this.register.password } 
+
+              
+
+               ).subscribe(data => {
                      console.log(data);
               });
                
