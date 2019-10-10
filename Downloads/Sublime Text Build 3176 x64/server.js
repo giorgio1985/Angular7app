f9acbb8f843d1f352755a6016d8f7c755cab6aaf
giorgio1985 <34795529+giorgio1985@users.noreply.github.com>
2019-10-11 00:03:31 +0200

    Last update

--- Downloads/Sublime Text Build 3176 x64/server.js
+++ Downloads/Sublime Text Build 3176 x64/server.js
@@ -1,15 +1,16 @@
 const http = require('http');
 const fs = require('fs');
 const express = require('express');
-var mysql = require('mysql');
 const app = express();
-const connection = require('./db');
+var mysql = require('mysql');
+
+//const conn = require('./db.js');
 
 const port = 3000;
 const mysqlPort = 3306;
 var bodyParser = require('body-parser');
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
@@ -17,48 +18,83 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 console.log('backend connected at port: ' + port);
 
-app.use('/signup',(req, res) => {
+  app.use('/signup',(req, res) => {
+
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");	 
 
- /* console.log('firstName: ' + req.body.firstName);
-  console.log('lastName: ' + req.body.lastName);
-  console.log('email: ' + req.body.email);
-  console.log('password :' + req.body.password);*/
+             firstname = req.body.firstName, 
+             lastname = req.body.lastName,
+             email = req.body.email,
+             password = req.body.password
+             console.log('Dati inseriti: ' + firstname + ' ' + lastname + ' ' + email + ' '+ password);
+             res.end();
+ var sql = "INSERT INTO registers (`firstName`, `secondName`,`email`,`password`) VALUES ('" + firstname + "','" + lastname + "', '" + email + "', '" + password + "')";
 
- var firstname = req.body.firstName;
- var lastname = req.body.lastName;
- var email = req.body.email;
- var password = req.body.password;
-console.log(require.body.firstName);
- connection.connect((err) => {
-	if (err) throw err;
-	else console.log('db connected');
-	 var sql = "INSERT INTO registers (firstName, secondName, email, password) VALUES ('firstname', 'lastname', 'email', 'password')";
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
+
+ var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root85',
+  database : 'database1'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
   });
-});
 
-  /*var obj = {
-  	         firstname: req.body.fistName, 
-             lastname: req.body.lastName,
-             email: req.body.email,
-             password: req.body.password
-              }
+ 
 
-     JsObj = (JSON.stringify(obj));*/
-});
-
-
-app.post('/signup', (req, res) => {
-	res.end('gugyggg');
+connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log("records inserted");
+   });
     
+connection.end();
+ });
+ app.use('/login', (req,res) => {
+  	  res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      email =  req.body.email;                          
+      password = req.body.password;                
+      console.log(email);
+      console.log(password);
+      res.end();
+
+      var sql = "SELECT firstName, secondName FROM registers WHERE email = '" + email + "' AND  password = '" + password + "' ";
+
+
+  var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root85',
+  database : 'database1'
 });
 
-app.listen(3000, function () {
-  console.log('service listening on port 3000!')
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  });
+
+ 
+
+connection.query(sql, (err, result) => {
+    if (err) throw err;
+    if (result) {
+    	    console.log("table selected");
+    console.log(result);
+} else if (result='') {
+	console.log('Result empty');
+}
+
+   });
+    
+connection.end();
+ });
+ 
+	app.listen(3000, function () {
+  console.log('service listening on port 3000!');
 });
 
 
